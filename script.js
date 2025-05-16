b9ede94945bd038cc7bcfe930be1bb5fdbd9d9ba
Austinnisot <tosinbobo2002@yahoo.co.uk>
2025-05-16 15:03:25 +0100

    webserver stage one

---
+++ script.js
@@ -0,0 +1,18 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 2em;
+  background-color: #f4f4f4;
+}
+
+header {
+  background: #333;
+  color: #fff;
+  padding: 1em;
+  text-align: center;
+}
+
+#projects {
+  margin-top: 2em;
+}
+
